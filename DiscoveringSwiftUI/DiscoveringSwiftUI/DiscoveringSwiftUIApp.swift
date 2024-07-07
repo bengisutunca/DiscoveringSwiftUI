29e90635f81c4b6d90b538322828b821e9d31635
Bengisu Tunca <bengitunca@gmail.com>
2024-07-07 20:28:10 +0300

    * Add project base

---
+++ DiscoveringSwiftUI/DiscoveringSwiftUI/DiscoveringSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoveringSwiftUIApp.swift
+//  DiscoveringSwiftUI
+//
+//  Created by Bengi Tunca on 7.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiscoveringSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
