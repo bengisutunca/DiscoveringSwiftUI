e15a5a8545c0832c3d6b97ada67ba7d2cd825858
Bengisu Tunca <bengitunca@gmail.com>
2024-07-13 21:56:37 +0300

    * Add project base

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bengi Tunca on 13.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
