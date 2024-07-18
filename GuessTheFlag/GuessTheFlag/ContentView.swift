c1e013769a9850cb2d69676b653e3bb62c189927
Bengisu Tunca <bengitunca@gmail.com>
2024-07-18 19:47:03 +0300

    * Used buttons and images

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-//        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
-//        LinearGradient(stops: [
-//            .init(color: .white, location: 0.45),
-//            .init(color: .black, location: 0.55),
-//        ], startPoint: .top, endPoint: .bottom)
-//        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
-//        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
-        Text("Your content")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .foregroundStyle(.white)
-            .background(.red.gradient)
+        VStack {
+            Button("Button 1") { }
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) { }
+                .buttonStyle(.bordered)
+            Button("Button 3") { }
+                .buttonStyle(.borderedProminent).tint(.mint)
+            Button("Button 4", role: .destructive) { }
+                .buttonStyle(.borderedProminent)
+            Button {
+                print("Button tapped.")
+            } label: {
+                Text("Button 5")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+            Button {
+                print("Edit button was tapped!")
+            } label: {
+                Label("Button 6", systemImage: "book")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.pink)
+            }
+            Button {
+                print("Edit button was tapped.")
+            } label: {
+                Image(systemName: "pencil")
+            }
+        }
     }
 }
 
