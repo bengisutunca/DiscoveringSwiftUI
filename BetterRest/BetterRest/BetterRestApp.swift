08610e704f2aa1e447cbb60298bb385aaff2f097
Bengisu Tunca <bengitunca@gmail.com>
2024-08-18 00:02:11 +0300

    * Add project base

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Bengi Tunca on 18.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
