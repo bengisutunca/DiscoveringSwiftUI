7878acd11326f4ccf280e4454d575f1d60904c1f
Bengisu Tunca <bengitunca@gmail.com>
2025-02-20 00:19:48 +0300

    * Add project base

---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Bengi Tunca on 20.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
