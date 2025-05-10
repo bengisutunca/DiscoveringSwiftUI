2e9888ba6b8393b63689c996ec18c68ec83c92f1
Bengisu Tunca <bengitunca@gmail.com>
2025-05-10 16:10:26 +0300

    * Implement basic word entry and list display in ContentView

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,23 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
 
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello World!")
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                }
+
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        Text(word)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
-        .padding()
     }
     
-    func testBundles() {
-        if let fileURL = Bundle.main.url(forResource: "some file", withExtension: "txt") {
-            /// found the file in the bundle.
-            if let fileContents = try? String(contentsOf: fileURL) {
-                /// loaded the file into string.
-            }
-        }
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
+
+        // extra validation to come
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
